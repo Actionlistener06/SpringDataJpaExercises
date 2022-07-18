d6a7d3b39d36c07424bc5c79ee873ba0497ba277
mustafa <mserhatdemirtas@gmail.com>
2022-07-18 06:43:19 +0300

    first commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student maria = new Student(
+                    "Maria","Jones","maria.jones@amigoscode.edu",21
+            );
+            studentRepository.save(maria);
+        };
+    }
+}
